aa8c36934949be0b5623b8f61cff7c4b5afb240b
sandro <sandrosilva9029@gmail.com>
2022-08-15 10:31:47 -0300

    findbyid implementado com tratamento de excecao

--- src/main/java/com/sandro/workshopmongodb/services/UserService.java
+++ src/main/java/com/sandro/workshopmongodb/services/UserService.java
@@ -1,20 +1,27 @@
 package com.sandro.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sandro.workshopmongodb.domain.User;
 import com.sandro.workshopmongodb.repositories.UserRepository;
+import com.sandro.workshopmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
