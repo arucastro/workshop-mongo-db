010c047fbf2e028b5d5a34482abac4cab337abd9
sandro <sandrosilva9029@gmail.com>
2022-08-15 14:22:21 -0300

    entidade post com user aninhado

---
+++ src/main/java/com/sandro/workshopmongodb/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.sandro.workshopmongodb.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sandro.workshopmongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
